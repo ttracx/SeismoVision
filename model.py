2453585ea922a5cb640241c9f747caaa8c7ffe46
ttracx <1948435-ttracx@users.noreply.replit.com>
2024-10-24 00:20:40 +0000

    Checkpoint after starting plan

--- model.py
+++ model.py
@@ -5,9 +5,18 @@ class SeismicClassifier:
     def __init__(self):
         self.model = None
         
-    def build_model(self, n_estimators=100):
+    def build_model(self, n_estimators=100, max_depth=None, min_samples_split=2,
+                   min_samples_leaf=1, max_features='auto', criterion='gini',
+                   class_weight=None):
+        """Build RandomForestClassifier with tunable hyperparameters"""
         model = RandomForestClassifier(
             n_estimators=n_estimators,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            max_features=max_features,
+            criterion=criterion,
+            class_weight=class_weight,
             random_state=42,
             n_jobs=-1
         )
@@ -15,7 +24,7 @@ class SeismicClassifier:
     
     def train(self, X, y, **kwargs):
         if self.model is None:
-            self.model = self.build_model()
+            self.model = self.build_model(**kwargs)
         
         # Reshape the input data if needed
         X = X.reshape(X.shape[0], -1)
@@ -44,3 +53,9 @@ class SeismicClassifier:
         # Get probabilities for each class
         probabilities = self.model.predict_proba(X)
         return probabilities
+
+    def get_feature_importance(self):
+        """Get feature importance scores from the model"""
+        if self.model is None:
+            raise ValueError("Model hasn't been trained yet!")
+        return self.model.feature_importances_
